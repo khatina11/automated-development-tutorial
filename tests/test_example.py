94f7ed2269d721db42aceb63e8e8a8d75abfcb6e
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2023-06-05 11:25:37 +0200

    add minimal unittest code

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,14 @@
 import unittest
+from automated_development_tutorial.example import fibonacci, sumOfPrimes
+
+
+class TestExample(unittest.TestCase):
+
+    def test_fibonacci(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_sum_of_primes(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
 
 if __name__ == '__main__':
